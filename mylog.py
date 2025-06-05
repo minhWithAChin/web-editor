58640d440df43c1d10194415801a802a59af4b91
Minh <minh.hung.le@tha.de>
2025-06-05 12:20:49 +0100

    for some reason it didn't stage mylog.py

---
+++ mylog.py
@@ -0,0 +1,26 @@
+
+import logging
+import os
+import sys
+
+log=logging.getLogger("myLog")
+log.setLevel(logging.DEBUG)
+formatter=logging.Formatter('%(asctime)s;%(levelname)s;%(module)s;%(funcName)s;%(message)s')
+formatter2=logging.Formatter('%(levelname)s - %(funcName)s: %(message)s')
+
+#https://stackoverflow.com/questions/35807605/create-a-file-if-it-doesnt-exist
+logFilePath = os.path.join(".","log_data","stocks.log")
+if not os.path.isfile(logFilePath):
+    os.mknod(codeFilePath)
+    log.warning("path für log code existiert nicht")
+
+logFileHandler=logging.FileHandler(logFilePath)
+logFileHandler.setFormatter(formatter)
+logFileHandler.setLevel(logging.DEBUG)
+
+logTerminalHandler=logging.StreamHandler(sys.stdout)
+logTerminalHandler.setFormatter(formatter2)
+logTerminalHandler.setLevel(logging.WARNING)
+
+log.addHandler(logFileHandler)
+log.addHandler(logTerminalHandler)
