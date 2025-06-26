99dbbfa89f02aa1aa9b385425cbad88d31af7831
Minh <minh.hung.le@tha.de>
2025-06-26 01:54:08 +0100

    implemented controls for car

--- student_code/test.py
+++ student_code/test.py
@@ -1,14 +1,2 @@
-import RPi.GPIO as GPIO
-import time
-
-GPIO.setmode (GPIO.BCM)
-GPIO.setup (3, GPIO.OUT)
-a=0
-
-while a<5:
-	GPIO.output(3, True)
-	time.sleep(1)
-	GPIO.output(3, False)
-	time.sleep(1)
-	a+=1
-	print(a)
+import subprocess
+subprocess.run("python3 mDev.py servo",shell=True,executable='/bin/bash' )
